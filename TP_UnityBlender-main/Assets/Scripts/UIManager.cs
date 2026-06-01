a318062d6b1af91398096af970c21c14c1f617d6
Alejo533 <meencantaelfutbol2010@gmail.com>
2026-06-01 14:25:18 -0300

    "Clase 3: HUD y Managers funcionales")

---
+++ TP_UnityBlender-main/Assets/Scripts/UIManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+
+    public TMP_Text score;
+    public TMP_Text timer;
+    public GameObject gameOvertext;
+
+    public void UpdateScore(int newscore)
+    {
+        score.text = "Score: " + newscore;
+    }
+
+    public void UpdateTimer(float newtime)
+    {
+        timer.text = "Tiempo: " + Mathf.Ceil(newtime);
+    }
+
+    public void ShowGameOver()
+    {
+        gameOvertext.SetActive(true);
+    }
+}
